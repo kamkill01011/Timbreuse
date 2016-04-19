04d24dd1d52d38509b1b00e1f721cff040d969f0
mathieu.jee <mathieu.jee@YV-E104-PC04.yv.cpnv.ch>
2016-04-19 14:50:04 +0200

    2 buttons

--- Test/src/ch/cpnv/timbreuse/servlets/ManageStudents.java
+++ Test/src/ch/cpnv/timbreuse/servlets/ManageStudents.java
@@ -15,22 +15,26 @@ import ch.cpnv.timbreuse.forms.StudentResearch;
 public class ManageStudents extends HttpServlet{
 	public static final String VIEW = "/WEB-INF/manageStudents.jsp";
 	private DAOUser daoUser;
-	
+
 	public void init() {
 		this.daoUser = ((DAOFactory) getServletContext().getAttribute("daofactory")).getDaoUser();
 	}
-	
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		this.getServletContext().getRequestDispatcher(VIEW).forward(request, response);
 	}
-	
+
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		StudentResearch researchForm = new StudentResearch(daoUser);
-		CreateStudent createForm = new CreateStudent(daoUser);
-		User user = researchForm.researchUser(request);
-		daoUser.create(createForm.isUser(request));
-		request.setAttribute("researchStudent", user);
+		if(request.getParameter("research")!=null) {
+			StudentResearch researchForm = new StudentResearch(daoUser);
+			User user = researchForm.researchUser(request);
+			request.setAttribute("researchStudent", user);
+		}
+		if(request.getParameter("add")!=null) {
+			CreateStudent createForm = new CreateStudent(daoUser);
+			daoUser.create(createForm.isUser(request));
+		}
 		this.getServletContext().getRequestDispatcher(VIEW).forward(request, response);
 	}
-	
+
 }
